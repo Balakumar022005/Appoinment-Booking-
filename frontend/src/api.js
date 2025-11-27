f7538f83c71ebc40626f749b877715ea1ba736f1
Balakumar Petchimuthu <balakumarpetchimuthu77@gmail.com>
2025-11-27 18:28:02 +0530

    Updated project including README

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,23 @@
 import axios from "axios";
 
+// Use Render backend URL from .env
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // Attach token automatically
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
